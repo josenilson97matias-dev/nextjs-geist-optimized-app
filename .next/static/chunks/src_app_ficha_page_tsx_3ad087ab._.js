4c4d6110db3f0fe9a67e72d3416b18549ce2c282
josenilson97matias-dev <josenilson97matias@gmail.com>
2025-08-20 22:51:33 -0300

    Add .next/static/chunks/src_app_ficha_page_tsx_3ad087ab._.js 1755741097575

---
+++ .next/static/chunks/src_app_ficha_page_tsx_3ad087ab._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_96d9829c._.js",
+  "static/chunks/node_modules_73c06505._.js"
+],
+    source: "dynamic"
+});
