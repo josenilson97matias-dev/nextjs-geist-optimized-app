2e31a63b89d196d271b467466fae97f734ddccc5
josenilson97matias-dev <josenilson97matias@gmail.com>
2025-08-20 22:51:29 -0300

    Add .next/static/chunks/src_app_instrucoes_page_tsx_3ad087ab._.js 1755741093319

---
+++ .next/static/chunks/src_app_instrucoes_page_tsx_3ad087ab._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_20d3fdff._.js",
+  "static/chunks/node_modules_b283f21b._.js"
+],
+    source: "dynamic"
+});
